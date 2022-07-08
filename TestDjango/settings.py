d1517223f8359702a1fa1c2f8e241d45eadff769
hikorigunpu <ju.valenzuelag@duocuc.cl>
2022-07-08 09:59:09 -0400

    asdadas

--- TestDjango/settings.py
+++ TestDjango/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'TestDjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/xepdb1',
+        'NAME': '127.0.0.1:1521/orcl',
         'USER': 'snail',
         'PASSWORD': '123',
         'TEST': {
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
